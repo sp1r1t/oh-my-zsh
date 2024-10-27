51a363204e84a9e4fd01002a0aaba621fcc779a2
Julian Konrath <julian.konrath@gmail.com>
2024-10-27 13:15:10 +0100

    add plugins install script

---
+++ plugins.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
